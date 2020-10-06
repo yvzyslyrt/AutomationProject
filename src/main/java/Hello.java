ef3c5a31325878bae456b5932aea9f0b84987f49
EU3 Batch <yavuzyesilyurt3@gmail.com>
2020-10-06 20:52:09 +0100

    complimant to eu3

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git EU3");
         //MORE CHANGES COMİNG
+
+        //eu3 is quite well
     }
 }
